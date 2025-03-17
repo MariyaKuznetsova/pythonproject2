e3daf4ae8d94530ad7d3bfd0107237b990fcc16a
MariyaKuznetsova <piratca93@mail.ru>
2025-03-17 21:09:02 +0400

    src/masks.py fixes

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,18 @@
-def get_mask_card_number(a: [str]) -> [str]:
+def get_mask_card_number(number_card: [str]) -> [str]:
     """Функцию маскировки номера банковской карты"""
-    number_card = input("Введите номер карты  ")
+    #number_card = input("Введите номер карты  ")
     number_card_new = f'{number_card[0:4]} {number_card[4:6]}** **** {number_card[12:]}'
     print(number_card_new)
 
+number_card = "84281762182468118"
+get_mask_card_number(number_card)
 
-get_mask_card_number([str])
 
-
-def get_mask_account(a: [str]) -> [str]:
+def get_mask_account(number_check: [str]) -> [str]:
     """Функцию маскировки номера банковского счета"""
-    number_check = input("Введите номер счета  ")
+    #number_check = input("Введите номер счета  ")
     number_check_new_s = f'**{number_check[15:]}'
     print(number_check_new_s)
 
-
-get_mask_account([str])
+number_check = "88718421712982152848"
+get_mask_account(number_check)
--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,18 @@
-import masks
-
-def mask_account_card(a: [str]) -> [str]:
+def mask_account_card(b: [str]) -> [str]:
     details_card = input("Введите данные: ")
     details_card_splits = details_card.split(" ")
-    print(details_card_splits)
-    details_card_new = f'{details_card_splits.isdigit()} {masks.get_mask_card_number(details_card_splits.isdigit())} '
-    print(details_card_new)
+    if details_card_splits[0] == "Счет":
+        print(details_card_splits[1])
+    else:
+        print(f'{details_card_splits[0:2]}')
+        #from masks import get_mask_account
+        #print(get_mask_account(details_card_splits[1]))
+    #name_card = details_card_splits.isalpha()
+    #details_card_new = f'{details_card_splits.isalpha()} {masks.get_mask_card_number(details_card_splits.isdigit())} '
+    #print(name_card)
 
 mask_account_card([str])
+
+
+#datetime.strptime
+#strftime
