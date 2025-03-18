8a5859eee99507eab2cd50b96262ba841b4526c4
MariyaKuznetsova <piratca93@mail.ru>
2025-03-18 22:16:18 +0400

    src/widget.py fixes

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,20 @@
-def get_mask_card_number(number_card: [str]) -> [str]:
+from typing import Union
+
+def get_mask_card_number(number_card: Union[str]) -> Union[str]:
     """Функцию маскировки номера банковской карты"""
-    #number_card = input("Введите номер карты  ")
     number_card_new = f'{number_card[0:4]} {number_card[4:6]}** **** {number_card[12:]}'
     return number_card_new
+
 
 number_card = "1596837868705199"
 get_mask_card_number(number_card)
 
 
-def get_mask_account(number_check: [str]) -> [str]:
+def get_mask_account(number_check: Union[str]) -> Union[str]:
     """Функцию маскировки номера банковского счета"""
-    #number_check = input("Введите номер счета  ")
     number_check_new_s = f'**{number_check[15:]}'
     return number_check_new_s
+
 
 number_check = "35383033474447895560"
 get_mask_account(number_check)
--- src/widget.py
+++ src/widget.py
@@ -1,23 +1,28 @@
-def mask_account_card(details_card: [str]) -> [str]:
+from typing import Union
+
+
+def mask_account_card(details_card: Union[str]) -> Union[str]:
+    """Функцию вывода номера карты или счета с функцией маскировки"""
     details_card_splits = details_card.split(" ")
-    from masks import get_mask_account
-    from masks import get_mask_card_number
+    from src.masks import get_mask_account, get_mask_card_number
     if details_card_splits[0] == "Счет":
-        print(f"{details_card_splits[0]} {get_mask_account(details_card_splits[1])}")
+        return f"{details_card_splits[0]} {get_mask_account(details_card_splits[1])}"
     else:
         if details_card_splits[0] == "Visa":
-            print(f"{details_card_splits[0]} {details_card_splits[1]} {get_mask_card_number(details_card_splits[-1])}")
+            return f"{details_card_splits[0]} {details_card_splits[1]} {get_mask_card_number(details_card_splits[-1])}"
         else:
-            print(f"{details_card_splits[0]} {get_mask_card_number(details_card_splits[-1])}")
+            return f"{details_card_splits[0]} {get_mask_card_number(details_card_splits[-1])}"
 
 
 details_card = "Счет 73654108430135874305"
-mask_account_card(details_card)
+print(mask_account_card(details_card))
 
-def get_date(date_str: [str]) -> [str]:
+
+def get_date(date_str: Union[str]) -> Union[str]:
+    """Функцию преобразования строки в строку с датой"""
     date_new = f'"{date_str[8:10]}.{date_str[5:7]}.{date_str[:4]}"'
-    print(date_new)
+    return date_new
 
 
 date_str = "2024-03-11T02:26:18.671407"
-get_date(date_str)
+print(get_date(date_str))
