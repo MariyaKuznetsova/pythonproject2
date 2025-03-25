e1db367ef69c4c96fb015476cea24149c37970aa
MariyaKuznetsova <piratca93@mail.ru>
2025-03-25 10:11:49 +0400

    homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -11,7 +11,7 @@ def filter_by_state(dictionary: List[Dict]) -> List[Dict]:
     return new_filter
 
 
-value_states = "CANCELED"
+value_states = "EXECUTED"
 dictionary = [
     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
