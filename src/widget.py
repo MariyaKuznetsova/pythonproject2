f0ed61efa4115989ea0c74b774e2c5efb7e2b7a5
MariyaKuznetsova <piratca93@mail.ru>
2025-04-02 00:00:13 +0400

    homework 10_2 done

--- src/widget.py
+++ src/widget.py
@@ -8,22 +8,28 @@ def mask_account_card(details_card: Union[str]) -> Union[str]:
 
     if details_card_splits[0] == "Счет":
         return f"{details_card_splits[0]} {get_mask_account(details_card_splits[1])}"
+    elif details_card == " ":
+        return "Неправильный формат данных"
+    elif details_card_splits[0] == "Visa":
+        return f"{details_card_splits[0]} {details_card_splits[1]} {get_mask_card_number(details_card_splits[-1])}"
     else:
-        if details_card_splits[0] == "Visa":
-            return f"{details_card_splits[0]} {details_card_splits[1]} {get_mask_card_number(details_card_splits[-1])}"
-        else:
-            return f"{details_card_splits[0]} {get_mask_card_number(details_card_splits[-1])}"
+        return f"{details_card_splits[0]} {get_mask_card_number(details_card_splits[-1])}"
 
 
-details_card = "Счет 73654108430135874305"
+details_card = "Visa Gold 599941422842635384882"
 print(mask_account_card(details_card))
 
 
 def get_date(date_str: Union[str]) -> Union[str]:
     """Функцию преобразования строки в строку с датой"""
-    date_new = f'"{date_str[8:10]}.{date_str[5:7]}.{date_str[:4]}"'
-    return date_new
+    if date_str == " ":
+        return "Неправильный формат данных"
+    elif date_str[4] and date_str[7] != "-":
+        return "Неправильный формат данных"
+    else:
+        date_new = f'"{date_str[8:10]}.{date_str[5:7]}.{date_str[:4]}"'
+        return date_new
 
 
-date_str = "2024-03-11T02:26:18.671407"
+date_str = "2025-04-01T02:26:18.653543"
 print(get_date(date_str))
