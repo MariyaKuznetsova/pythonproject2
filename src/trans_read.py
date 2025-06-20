5afe88551e9dcb6eb797ba5b1e47f6efad1ea441
MariyaKuznetsova <piratca93@mail.ru>
2025-06-21 02:18:04 +0400

    homework 13_1 done

---
+++ src/trans_read.py
@@ -0,0 +1,31 @@
+import csv
+from typing import Dict, List, Union
+
+import pandas as pd
+
+
+def func_csv_(csv_file: Union[str]) -> List[Dict]:
+    try:
+        with open(csv_file, encoding='utf-8') as trans_file:
+            reader = csv.DictReader(trans_file, delimiter=';')
+            list_dictionary = []
+            for row in reader:
+                list_dictionary.append(row)
+            return list_dictionary
+    except Exception:
+        return []
+
+
+#print(func_csv_("../data/trans_read_test.csv"))
+
+
+def excel_func(excel_file: Union[str]) -> List[Dict]:
+    try:
+        excel_trans = pd.read_excel(excel_file)
+        dictionary = excel_trans.to_dict('records')
+        return dictionary
+    except Exception:
+        return []
+
+
+#print(excel_func("../data/transactions_excel_test.xlsx"))
---
+++ tests/test_trans_read.py
@@ -0,0 +1,94 @@
+from unittest.mock import Mock, mock_open, patch
+
+import pytest
+import src.trans_read
+
+
+def test_func_csv_():
+    mock_csv_file = Mock(
+        return_value=[
+            {
+                "id": "650703",
+                "state": "EXECUTED",
+                "date": "2023-09-05T11:30:32Z",
+                "amount": "16210",
+                "currency_name": "Sol",
+                "currency_code": "PEN",
+                "from": "Счет 58803664561298323391",
+                "to": "Счет 39745660563456619397",
+                "description": "Перевод организации",
+            }
+        ]
+    )
+    func_csv_ = mock_csv_file
+    assert func_csv_() == [
+        {
+            "id": "650703",
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": "16210",
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
+        }
+    ]
+    mock_csv_file.assert_called_with()
+
+
+@patch(
+    "src.trans_read.open",
+    new_callable=mock_open,
+    read_data='id;state;date\n650703;EXECUTED;2023-09-05T11:30:32Z\n',
+)
+
+def test_func_csv_1(mock_csv_file):
+    result_func_1 = src.trans_read.func_csv_("../data/trans_read_test.csv")
+    assert result_func_1 == [{'id': '650703', 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z'}]
+    mock_csv_file.assert_called_with("../data/trans_read_test.csv", encoding="utf-8")
+
+
+def test_excel_func():
+    mock_excel_file = Mock(
+        return_value=[
+            {
+                "id": "650703",
+                "state": "EXECUTED",
+                "date": "2023-09-05T11:30:32Z",
+                "amount": "16210",
+                "currency_name": "Sol",
+                "currency_code": "PEN",
+                "from": "Счет 58803664561298323391",
+                "to": "Счет 39745660563456619397",
+                "description": "Перевод организации",
+            }
+        ]
+    )
+    excel_func = mock_excel_file
+    assert excel_func() == [
+        {
+            "id": "650703",
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": "16210",
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
+        }
+    ]
+    mock_excel_file.assert_called_with()
+
+
+@patch(
+    "src.trans_read.open",
+    new_callable=mock_open,
+    read_data='id;state;date\n650703;EXECUTED;2023-09-05T11:30:32Z\n',
+)
+
+def test_excel_func_1(mock_excel_file):
+    result_func_2 = src.trans_read.excel_func("../data/transactions_excel_test.xlsx")
+    assert result_func_2 == [{'id': 650703, 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z'}]
+    mock_excel_file.assert_called_once_with("../data/transactions_excel_test.xlsx")
